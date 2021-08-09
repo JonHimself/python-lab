134df2cbf9ec5d9ab8396a723f82e3fca0b1d4dc
JonHimself <jcbrstudio@gmail.com>
2021-08-09 17:31:03 -0500

    Finsihed Python Lab

--- data-python/data_presnter.py
+++ data-python/data_presnter.py
@@ -2,15 +2,21 @@ open_file = open("CupcakeInvoices.csv")
 for type in open_file:
     type = type.split(",")
     print(type[2])
-open_file.close()
 
-open_file = open("CupcakeInvoices.csv")
+
+open_file.seek(0, 0)  # run after using a for loop on an open file to reset it
+
+
 purchase = []
+
 for row in open_file:
     row = row.rstrip('\n').split(",")
     row_total = float(row[3]) * float(row[4])
     purchase.append(row_total)
+
 print(purchase)
-purchase_total = sum(purchase)
+
+purchase_total = round(sum(purchase), 2)
 print(purchase_total)
+
 open_file.close()
